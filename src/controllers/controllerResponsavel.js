2bfb148d1347da26d71fddbf051e36e4a57e6261
tudo-bem-autismo <lukassvenancio022@gmail.com>
2022-09-16 14:28:46 -0300

    Done: Sistema de alteração de senha.

--- src/controllers/controllerResponsavel.js
+++ src/controllers/controllerResponsavel.js
@@ -162,8 +162,9 @@ exports.put = async (req, res, next) =>{
             data.senha = encryptedSenha;
 
         }
-
     }
+
+    
 
     prisma.tbl_responsavel.update({
 
@@ -218,6 +219,132 @@ exports.put = async (req, res, next) =>{
             }
         }
     )
+}
+
+exports.putPassword = (req, res, next) =>{
+
+    const id = req.params.id;
+    const data = req.body;
+
+    if(data.senha && data.senha_atual){
+
+        // console.log('entreiii')
+
+        prisma.tbl_responsavel.findUnique({
+
+            where:{
+    
+                id: parseInt(id)
+            }
+        
+        }).then(
+    
+            (responsavel) =>{
+    
+                const crypto = require('../config/globalFunctions');
+                const encryptedSenha_atual = crypto.encrypt(data.senha_atual);
+                const encryptedSenha = crypto.encrypt(data.senha);
+                
+                if(encryptedSenha && encryptedSenha_atual){
+
+                    data.senha_atual = encryptedSenha_atual;
+                    data.senha = encryptedSenha;
+    
+                    if(responsavel.senha == data.senha_atual){
+
+                        prisma.tbl_responsavel.update({
+        
+                            data:{
+    
+                                senha: data.senha
+                            },
+                            where:{
+    
+                                id: parseInt(id)
+                            }
+    
+                            
+                        }).then(
+    
+                            () => {
+    
+                                res.status(200).json({
+                                    message: "Senha atualizada com sucesso!"
+                                });
+    
+                            }
+                        
+                        ).catch(
+    
+                            (error) => {
+    
+                                if(error.code == "P2025"){
+    
+                                    res.status(400).send({message: "ID não encontrado na base de dados."});
+                                
+                                
+                                }else{
+                    
+                                    const argument = error.message.split('Argument')[1]
+                    
+                                    if(argument){
+                    
+                                        const err = argument.split('\n')[0]
+                    
+                                        res.status(400).json({"message" : err});
+                    
+                                    }else{
+                    
+                                        res.status(500).json({"message" : error});
+                    
+                                    }   
+                                }
+                            }
+                        )
+                    
+                    }else{
+
+                        res.status(400).json({message: 'Senha atual incorreta.'})
+                    }
+                
+                }else{
+
+                    res.status(400).json({message: 'Não foi possível encriptar a senha.'})
+                }
+            }
+        
+        ).catch(
+    
+            (error) => {
+
+                if(error.code == "P2025"){
+
+                    res.status(400).send({message: "ID não encontrado na base de dados."});
+                
+                
+                }else{
+    
+                    const argument = error.message.split('Argument')[1]
+    
+                    if(argument){
+    
+                        const err = argument.split('\n')[0]
+    
+                        res.status(400).json({"message" : err});
+    
+                    }else{
+    
+                        res.status(500).json({"message" : error});
+    
+                    }   
+                }
+            }
+        )
+    
+    }else{
+
+        res.status(400).json({message: 'Dados inválidos'})
+    }
 }
 
 exports.delete = (req, res, next) =>{
--- src/routes/routesResponsavel.js
+++ src/routes/routesResponsavel.js
@@ -6,9 +6,10 @@ const controller = require('../controllers/controllerResponsavel');
 
 router.post('/', controller.post);
 router.put('/:id', controller.put);
+router.put('/senha/:id', controller.putPassword);
 router.delete('/:id', controller.delete);
 router.get('/', controller.get);
 router.get('/:id', controller.getById);
-router.post('/login', controller.login)
+router.post('/login', controller.login);
 
 module.exports = router;
