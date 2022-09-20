eb5069e26e2433e379a6f4c49df71160542cd083
Florbela <tbautismo22@gmail.com>
2022-09-20 13:48:49 -0300

    Update routesCrianca.js

--- src/routes/routesCrianca.js
+++ src/routes/routesCrianca.js
@@ -5,9 +5,17 @@ const router = express.Router();
 const controller = require('../controllers/controllerCrianca')
 
 const multer = require('multer');
-const multerConfig = require('../config/multer');
+// const multerConfig = require('../config/multer');
+const { memoryStorage } = require('multer');
 
-router.post('/', multer(multerConfig).single("arquivo"), controller.post);
+const Multer = multer({
+    storage: memoryStorage,
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+});
+
+router.post('/', Multer.single("arquivo"), controller.post);
 router.get('/', controller.get);
 router.get('/:id', controller.getById);
 router.put('/:id', controller.put);
