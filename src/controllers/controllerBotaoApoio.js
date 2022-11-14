e3a1a956a6d7555c1b1ef0f996540226ec42bb37
Lukas Santos Venancio <88518524+LukasVenancio@users.noreply.github.com>
2022-11-14 02:47:24 -0300

    Done: Endpoint de post de Botão de apoio funcionando

--- src/controllers/controllerBotaoApoio.js
+++ src/controllers/controllerBotaoApoio.js
@@ -3,20 +3,76 @@
 const prisma = require('../prismaClient');
 const firebase = require('../../services/firebase')
 
-exports.post = (req, res) => {
+exports.post = async (req, res) => {
 
     try {
 
+        const id = parseInt(req.body.id_crianca)
         const files = req.files
 
         const uploads = files.map((file) => {
             return firebase.uploadFiles(file)
         })
 
-        res.status(200).json(uploads)
+        const types = await prisma.tbl_tipo_midia.findMany()
+
+        const media = uploads.map(
+
+            (upload) => {
+
+                const type = types.find(
+
+                    (type) => type.mimetype == upload.mimetype
+
+                )
+
+                return {
+                    id_crianca: id,
+                    midia: upload.link,
+                    nome_original: upload.originalname,
+                    id_tipo_midia: parseInt(type.id)
+                }
+            }
+        )
+        console.log(media)
+
+        prisma.tbl_botao_apoio.createMany({
+
+            data: media
+        })
+            .then(() => { res.status(201).json() })
+            .catch((error) => {
+
+                if (error.code == 'P2003') {
+
+                    res.status(400).json({ message: `Chave estrangeira inválida no campo ${error.meta.field_name}` })
+
+                } else if (error.code == "P2000") {
+
+                    res.status(500).json({ message: `A quantidade máxima de caracteres foi ultrapassada no campo ${error.meta.column_name}.` })
+
+                } else {
+
+                    const argument = error.message.split('Argument')[1]
+
+                    if (argument) {
+
+                        const err = argument.split('\n')[0]
+
+                        res.status(400).json({ "message": err });
+
+                    } else {
+
+                        res.status(500).json({ "message": error });
+
+                    }
+                }
+            })
 
     } catch (error) {
 
-        throw error
+        const e = new Error(error)
+        console.log(e)
+        res.status(500).json(error)
     }
 } 
