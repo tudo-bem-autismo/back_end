a383b8b580a213596ff6f94cc8238953b03b0fbe
Florbela <tbautismo22@gmail.com>
2022-11-23 16:50:43 -0300

    In progress: API agenda - upload
    Criando objetos no postman

--- src/controllers/controllerIcone.js
+++ src/controllers/controllerIcone.js
@@ -1,32 +1,63 @@
 'use strict';
 
 const prisma = require('../prismaClient');
+const firebase = require('../../services/firebase')
 
-exports.post = (req, res, next) => {
+exports.post = async (req, res, next) => {
 
-    const data = req.body;
+    console.log(req.body)
+    // try {
+    //     const data = req.body;
+    //     const files = req.files
 
-    const {firebaseUrl} = req.file ? req.file : "";
+    //     const uploads = files.map((file) => {
+    //         return firebase.uploadImages(file)
+    //     })  
 
-    prisma.tbl_icone.create({
-        data:{
-            icone: firebaseUrl,
-            titulo: data.titulo
-        },
-        select:{
-            id: true,
-            icone: true
-        }
-    }).then(
-        (icone) => {
-            res.status(201).json(icone);
-        }
-    ).catch(
-        (error) => {
-            res.status(500).json({ "message": error })
-        }
-    )
+    //     prisma.tbl_icone.createMany({
+    //         data: 
+    //     }).then(
+    //         (icone) => {
+    //             res.status(201).json(icone);
+    //         }
+    //     ).catch(
+    //         (error) => {
+    //             console.log(error);
+    //             res.status(500).json({ "message": error })
+    //         }
+    //     )
+    // } catch (error) {
+    //     const e = new Error(error)
+    //     console.log(e)
+    //     res.status(500).json(error)
+    // }
 }
+
+// exports.post = (req, res, next) => {
+
+//     const data = req.body;
+
+//     const {firebaseUrl} = req.file ? req.file : "";
+
+//     prisma.tbl_icone.create({
+//         data:{
+//             icone: firebaseUrl,
+//             titulo: data.titulo
+//         },
+//         select:{
+//             id: true
+//         }
+//     }).then(
+//         (icone) => {
+//             res.status(201).json(icone);
+//         }
+//     ).catch(
+//         (error) => {
+//             console.log(error);
+//             res.status(500).json({ "message": error })
+//         }
+//     )
+// }
 
 exports.get = (req, res) =>{
 
