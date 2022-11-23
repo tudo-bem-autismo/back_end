7f2dd01e979fe7490bdec74769b920fde5aba32d
tudo-bem-autismo <lukassvenancio022@gmail.com>
2022-11-23 16:45:36 -0300

    In progress : faltando validacao para ids de criancas multiplos

--- src/controllers/controllerTarefa.js
+++ src/controllers/controllerTarefa.js
@@ -2,17 +2,36 @@
 
 const prisma = require('../prismaClient');
 
-exports.post = (req, res, next) => {
+exports.post = async (req, res, next) => {
 
     const data = req.body
+    // console.log(typeof(data.id_crianca.length()))
+    
+    if(typeof(data.id_crianca) == 'object'){
+        console.log(data)
+    }
+    
 
-    prisma.tbl_crianca_tarefa.create({
-        data:{
-            id_crianca: parseInt(data.id_crianca),
-            tbl_tarefa:{
-                
+
+
+    prisma.tbl_tarefa.create({
+        data: {
+            titulo: data.titulo,
+            horario: data.horario,
+            id_icone: parseInt(data.id_icone),
+            tbl_crianca_tarefa: {
+                createMany: {
+                    id_crianca: parseInt(data.id_crianca)
+                }
+            },
+            tbl_tarefa_dia_semana: {
+                create: {
+                    id_dia_semana: parseInt(data.id_dia_semana)
+                }
             }
-           
+        },
+        select: {
+            id: true
         }
     }).then(
         (tarefa) => {
@@ -20,44 +39,53 @@ exports.post = (req, res, next) => {
         }
     ).catch(
         (error) => {
+            console.log(error)
             res.status(500).json({ "message": error })
         }
     )
 }
 
-exports.get = (req, res) =>{
+exports.get = (req, res) => {
 
     const id_crianca = req.params.id;
 
     prisma.tbl_crianca_tarefa.findMany({
-        where:{
+        where: {
             id_crianca: parseInt(id_crianca)
         },
-        include:{
-            tbl_tarefa: true,
-            tbl_icone: true,
-            tbl_dia_semana: true
+        include: {
+            tbl_tarefa:{
+                include:{
+                    tbl_icone: true,
+                    tbl_tarefa_dia_semana:{
+                        include:{
+                            tbl_dia_semana:true
+                        }
+                    }
+                }
+            }
         }
     }).then(
         (data) => {
             res.status(200).json(data);
         }
     )
-    .catch(
-        (error) => {
-            res.status(500).json({ "message": error })
-        }
-    )
+        .catch(
+            (error) => {
+                console.log(error)
+                res.status(500).json({ "message": error })
+            }
+        )
 }
 
-exports.getById = (req, res, next) =>{
+exports.getById = (req, res, next) => {
 
     const prisma = require('../prismaClient');
 
     const id = req.params.id;
 
     prisma.tbl_crianca_tarefa.findUnique({
-        where:{
+        where: {
             id: parseInt(id)
         }
     }).then(
@@ -69,7 +97,7 @@ exports.getById = (req, res, next) =>{
             const argument = error.message.split('Argument')[1]
             const err = argument.split('\n')[0]
 
-            res.status(400).json({"message" : err});
+            res.status(400).json({ "message": err });
         }
     );
 }
