ee4ada44f2345229d6089c0c5e1e8bc9a5f790c7
Florbela <tbautismo22@gmail.com>
2022-11-25 16:51:38 -0300

    In progress: API agenda - post

--- src/controllers/controllerIcone.js
+++ src/controllers/controllerIcone.js
@@ -1,63 +1,30 @@
 'use strict';
 
 const prisma = require('../prismaClient');
-const firebase = require('../../services/firebase')
 
-exports.post = async (req, res, next) => {
+exports.post = (req, res, next) => {
 
-    console.log(req.body)
-    // try {
-    //     const data = req.body;
-    //     const files = req.files
+    const data = req.body;
+    
+    const icones = data.icones.map((icone) => {
 
-    //     const uploads = files.map((file) => {
-    //         return firebase.uploadImages(file)
-    //     })  
-
-    //     prisma.tbl_icone.createMany({
-    //         data: 
-    //     }).then(
-    //         (icone) => {
-    //             res.status(201).json(icone);
-    //         }
-    //     ).catch(
-    //         (error) => {
-    //             console.log(error);
-    //             res.status(500).json({ "message": error })
-    //         }
-    //     )
-    // } catch (error) {
-    //     const e = new Error(error)
-    //     console.log(e)
-    //     res.status(500).json(error)
-    // }
+        prisma.tbl_icone.createMany({
+            data:{
+                icone: icone.icone,
+                titulo: icone.titulo
+            }
+        }).then(
+            (data) =>{
+                res.status(200).json(data);
+            }
+        ).catch(
+            (error) => {
+                // console.log(error);
+                res.status(500).json({ "message": error });
+            }
+        )
+    }) 
 }
-
-// exports.post = (req, res, next) => {
-
-//     const data = req.body;
-
-//     const {firebaseUrl} = req.file ? req.file : "";
-
-//     prisma.tbl_icone.create({
-//         data:{
-//             icone: firebaseUrl,
-//             titulo: data.titulo
-//         },
-//         select:{
-//             id: true
-//         }
-//     }).then(
-//         (icone) => {
-//             res.status(201).json(icone);
-//         }
-//     ).catch(
-//         (error) => {
-//             console.log(error);
-//             res.status(500).json({ "message": error })
-//         }
-//     )
-// }
 
 exports.get = (req, res) =>{
 
--- src/routes/routesIcone.js
+++ src/routes/routesIcone.js
@@ -4,14 +4,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controllerIcone');
 
-const multer = require("multer");
-
-const Multer = multer({
-    storage: multer.memoryStorage(),
-    limits: 5 * 1024 * 1024,
-});
-
-router.post('/', Multer.any(), controller.post);
+router.post('/', controller.post);
 router.get('/', controller.get);
 
 module.exports = router;
