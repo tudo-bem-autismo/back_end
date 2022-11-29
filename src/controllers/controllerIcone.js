2921af23403502c27df26bdf2f4253fdd06e25de
Lukas Santos Venancio <tbautismo22@gmail.com>
2022-11-29 12:15:36 -0300

    Atualizando ícones

--- src/controllers/controllerIcone.js
+++ src/controllers/controllerIcone.js
@@ -2,28 +2,57 @@
 
 const prisma = require('../prismaClient');
 
+// exports.post = (req, res, next) => {
+
+    // const data = req.body;
+    
+    // const icones = data.icones.map((icone) => {
+
+    //     prisma.tbl_icone.createMany({
+    //         data:{
+    //             icone: icone.icone,
+    //             titulo: icone.titulo
+    //         }
+//         }).then(
+//             (data) =>{
+//                 res.status(200).json(data);
+//             }
+//         ).catch(
+//             (error) => {
+//                 // console.log(error);
+//                 res.status(500).json({ "message": error });
+//             }
+//         )
+//     }) 
+// }
 exports.post = (req, res, next) => {
 
-    const data = req.body;
-    
-    const icones = data.icones.map((icone) => {
+    try{
 
-        prisma.tbl_icone.createMany({
-            data:{
-                icone: icone.icone,
-                titulo: icone.titulo
-            }
-        }).then(
-            (data) =>{
-                res.status(200).json(data);
-            }
-        ).catch(
-            (error) => {
-                // console.log(error);
-                res.status(500).json({ "message": error });
-            }
-        )
-    }) 
+        const data = req.body;
+
+        const icones = data.icones.map(async (icone) => {
+
+            const icon = await prisma.tbl_icone.createMany({
+                data:{
+                    icone: icone.icone,
+                    titulo: icone.titulo
+                }
+                
+            })
+
+            return icon
+
+        })
+
+        res.status(201).json(icones)
+
+    }catch(error){
+
+        const e = new Error(error)
+        res.status(500).json(e.message)
+
+    }
 }
 
 exports.get = (req, res) =>{
